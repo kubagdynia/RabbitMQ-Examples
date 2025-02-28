12a31267825dee1b211c8bcdc21bf6f721391328
kubagdynia <kubagdynia@gmail.com>
2025-02-28 21:36:39 +0100

    Cluster config

--- Infra/RabbitMQ-Cluster-Docker/down.bat
+++ Infra/RabbitMQ-Cluster-Docker/down.bat
@@ -1,3 +1,4 @@
 docker-compose -p rabbitmq-cluster -f rabbitmq-cluster.yaml down
-docker volume rm rabbitmq-cluster_rabbitmq-node-1-data
-docker volume rm rabbitmq-cluster_rabbitmq-node-2-data
+docker volume rm rabbitmq-cluster_rabbitmq-cluster-node-1-data
+docker volume rm rabbitmq-cluster_rabbitmq-cluster-node-2-data
+docker volume rm rabbitmq-cluster_rabbitmq-cluster-node-3-data
--- Infra/RabbitMQ-Cluster-Docker/readme.md
+++ Infra/RabbitMQ-Cluster-Docker/readme.md
@@ -1,2 +1,3 @@
 docker exec -it rabbitmq-example rabbitmqctl version
 
+docker exec -it rabbitmq-cluster-node-1 rabbitmqctl hash_password adminadmin # Hqm0iyKF8znYkmJIBNUnje5EUKvvTeK2H6v6viny3ujaZhEZ
